07d4d99923e312e743d6d0d4226e494b7add1305
Hafis <johndoe@example.com>
2024-07-08 11:40:57 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+const PORT = process.env.PORT || 3000;
+const IP = process.env.IP || '0.0.0.0';
+app.listen(PORT, IP, () => console.log(`server running on ${PORT} 🏃🏻‍♀️`))
